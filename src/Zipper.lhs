019de425c009a872ec25c5db46adb04ee24af86b
Stephen Webber <montokapro@gmail.com>
2021-01-30 22:53:35 -0600

    Initial zipper implementation

---
+++ src/Zipper.lhs
@@ -0,0 +1,39 @@
+This code is heavily inspired by:
+https://github.com/srijs/haskell-lambda-term-zipper/blob/master/src/Data/Lambda/Term/Zipper.hs
+
+\begin{code}
+module Zipper (Context(..), Location(..), zipper, down, up, left, right) where
+
+import qualified LambdaCalculus as L
+
+data Context
+  = Top
+  | Abs Context
+  | AppL Context L.Expr
+  | AppR L.Expr Context
+  deriving (Eq, Show)
+
+data Location = At { focus :: L.Expr, with :: Context } deriving (Eq, Show)
+
+zipper :: L.Expr -> Location
+zipper expr = At expr Top
+
+down :: Location -> Location
+down (At (L.Abs a) context) = At a (Abs context)
+down (At (L.App a b) context) = At a (AppL context b)
+down location = location
+
+up :: Location -> Location
+up (At a (Abs context)) = At (L.Abs a) context
+up (At a (AppL context b)) = At (L.App a b) context
+up (At a (AppR b context)) = At (L.App b a) context
+up location = location
+
+left :: Location -> Location
+left (At a (AppR b context)) = At b (AppL context a)
+left location = location
+
+right :: Location -> Location
+right (At a (AppL context b)) = At b (AppR a context)
+right location = location
+\end{code}
---
+++ test/ZipperSpec.hs
@@ -0,0 +1,43 @@
+module ZipperSpec (spec) where
+
+import Test.Hspec
+import qualified LambdaCalculus as L
+import qualified Zipper as Z
+
+spec :: Spec
+spec = do
+  it "zipper" $ do
+    Z.zipper (L.Var 1)
+      `shouldBe` Z.At (L.Var 1) Z.Top
+
+  describe "down" $ do
+    it "abs" $ do
+      Z.down (Z.zipper (L.Abs (L.Var 1)))
+        `shouldBe` Z.At (L.Var 1) (Z.Abs Z.Top)
+
+    it "app" $ do
+      Z.down (Z.zipper (L.App (L.Var 1) (L.Var 2)))
+        `shouldBe` Z.At (L.Var 1) (Z.AppL Z.Top (L.Var 2))
+
+  describe "up" $ do
+    it "abs" $ do
+      Z.up (Z.At (L.Var 1) (Z.Abs Z.Top))
+        `shouldBe` Z.zipper (L.Abs (L.Var 1))
+
+    it "left app" $ do
+      Z.up (Z.At (L.Var 1) (Z.AppL Z.Top (L.Var 2)))
+        `shouldBe` Z.zipper (L.App (L.Var 1) (L.Var 2))
+
+    it "right app" $ do
+      Z.up (Z.At (L.Var 2) (Z.AppR (L.Var 1) Z.Top))
+        `shouldBe` Z.zipper (L.App (L.Var 1) (L.Var 2))
+
+  describe "left" $ do
+    it "app" $ do
+      Z.left (Z.At (L.Var 2) (Z.AppR (L.Var 1) Z.Top))
+        `shouldBe` Z.At (L.Var 1) (Z.AppL Z.Top (L.Var 2))
+
+  describe "right" $ do
+    it "app" $ do
+      Z.right (Z.At (L.Var 1) (Z.AppL Z.Top (L.Var 2)))
+        `shouldBe` Z.At (L.Var 2) (Z.AppR (L.Var 1) Z.Top)
